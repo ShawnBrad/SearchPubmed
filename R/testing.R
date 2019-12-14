97bc17f08a8c0813f761b41ac951840283e54e1c
Shawn <shawn_williams@brown.edu>
2019-12-14 18:57:49 -0500

    trying to fix lazyload issue

--- R/testing.R
+++ R/testing.R
@@ -7,16 +7,31 @@
 library(devtools)
 
 library(dplyr)
+usethis::edit_r_environ()
 install_github("ShawnBrad/SearchPubmed", force = T)
 library(PubSearch)
 
 
-saved.search <- search.pubmed(term = 'cell')
+saved.search <- search.pubmed(term = 'cell',retmax = 200, dates = c(2015,2019))
 
 open.article('Postnatal', results = saved.search)
 
 
 
-open.article("Children's", results = saved.search, index = 'article.abstracts')
+
+filter.tags <-  readRDS('R/sysdata.rds')
+use_data(filter.tags, filter.tags, internal = TRUE) 
+
+
+GITHUB_PAT <- 'c0f2a832805c5fa7f6a0d0448f0a7e635432d499'
+
+
+usethis::edit_r_environ(scope = 'user',) 
+
+
+
+
+usethis::edit_r_environ()
+
 
 
