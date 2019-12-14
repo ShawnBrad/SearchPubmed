4b7212692355eebef34b41847ecaeff44ecb8953
Shawn <shawn_williams@brown.edu>
2019-12-14 15:52:51 -0500

    fixed erroneous error, was eevaluating value of index to check number of articles to open rather than the length

--- R/open.article.R
+++ R/open.article.R
@@ -11,7 +11,7 @@ open.article <- function(search = NULL, results = NULL, index = 'article.titles'
     record.id <- results[indx,] %>%
       pull(article.pubmedID)
 
-    if( record.id > 1) (warning( 'search matched multiple records, using the first match'))
+    if( length(record.id) > 1) (warning( 'search matched multiple records, using the first match'))
     record.id <-  record.id[1]
     paste0('https://www.ncbi.nlm.nih.gov/pubmed/',record.id) %>%
       browseURL()
