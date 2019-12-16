7b8cd989aa0c1a599c2a2e12e6e541820a0e79a7
Shawn <shawn_williams@brown.edu>
2019-12-16 02:49:18 -0500

    revision

--- R/article.scrape.R
+++ R/article.scrape.R
@@ -33,7 +33,7 @@ article.scrape <- function(parsed.xml){
     xml_text()
 
   ### formatting output tibble
-  as_tibble(article.date,
+  tibble(article.date,
          article.titles,
          article.abstracts,
          article.types,
