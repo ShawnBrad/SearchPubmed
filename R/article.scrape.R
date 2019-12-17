58e85afdf8db2716cec462dc92fc5db1e7dfee1b
Shawn <shawn_williams@brown.edu>
2019-12-17 17:33:14 -0500

    add check for no results and set to NA to prevent tibble creation from breaking

--- R/article.scrape.R
+++ R/article.scrape.R
@@ -5,11 +5,13 @@ article.scrape <- function(parsed.xml){
 
     article.titles <- parsed.xml %>%
       get.article.title()
+    if (is.null(article.titles)) article.titles = NA
 
 
   # Get abstracts
   article.abstracts <- parsed.xml %>%
     get.article.abstracts()
+  if (is.null(article.abstracts)) article.abstracts = NA
 
   # Get Authors
   # list of tibbles
@@ -19,18 +21,22 @@ article.scrape <- function(parsed.xml){
   #Get journal
   article.journal <- parsed.xml %>%
     get.article.journal()
+  if (is.null(article.journal)) article.journal = NA
 
   #Get pubdate
   article.date <- parsed.xml %>%
     get.article.date()
+  if (is.null(article.date)) article.date = NA
 
   #Get article types
    article.types <- parsed.xml %>%
      get_using_get.article.types()
+   if (is.null(article.types)) article.types = NA
 
   # Get pubmed id
   article.pubmedID <- xml_nodes(parsed.xml,xpath =  './/MedlineCitation/PMID') %>%
     xml_text()
+  if (is.null(article.pubmedID)) article.pubmedID = NA
 
   ### formatting output tibble
   tibble(article.date,
