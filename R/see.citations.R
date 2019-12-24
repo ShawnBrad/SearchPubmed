a9480a724ce7b6f741372968de38bbca3417991b
Shawn <shawn_williams@brown.edu>
2019-12-24 18:14:17 -0500

    created function as wrapper to cr_cn to use cross ref to get formatted citations for pubmed ids

---
+++ R/see.citations.R
@@ -0,0 +1,18 @@
+see.citations <- function(search = NULL, results = NULL , index = 'article.titles', ...){
+  
+  if (!is.null(search)){
+    if (is.null(results)) stop('result table not provided ')
+    
+    indx <- results %>%
+      pull(index) %>%
+      str_which(search)
+    
+    if (is.null(indx)) stop("could not find matching search in index")
+    
+    results[indx,] %>%
+      pull(article.pubmedID) %>%
+      get.citations(...) %>%
+      return()
+    
+  }
+}
